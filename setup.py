93c2c938a9fa0dd3bca41f7066c4fe9c141aee04
Ashley Felton <ashley@ropable.com>
2016-05-18 12:18:31 +0800

    Update SSOLoginMiddleware to allow custom User models.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(name='dpaw-utils',
-      version='0.3a10',
+      version='0.3a11',
       description='Utilities for Django/Python apps',
       url='https://bitbucket.org/dpaw/dpaw-utils',
       author='Department of Parks and Wildlife',
       author_email='asi@dpaw.wa.gov.au',
       license='BSD',
-      packages=['dpaw_utils','dpaw_utils.requests'],
-      install_requires=["django", "requests", "bottle", "django-confy", "ipython",
-          "django-extensions", "gevent", "django-uwsgi", "django-redis", "psycopg2"],
+      packages=['dpaw_utils', 'dpaw_utils.requests'],
+      install_requires=[
+          'django', 'requests', 'bottle', 'django-confy', 'ipython',
+          'django-extensions', 'gevent', 'django-uwsgi', 'django-redis', 'psycopg2'],
       zip_safe=False)
-
