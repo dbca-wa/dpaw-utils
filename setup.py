a878c2eeb8788620b4ba61d2c3acf82c385d4790
Ashley Felton <ashley@ropable.com>
2018-03-22 13:26:37 +0800

    Bugfix SSOLoginMiddleware.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='dpaw-utils',
-      version='0.4.1',
+      version='0.4.2',
       description='Utilities for Django/Python apps',
-      url='https://github.com/parksandwildlife/dpaw-utils',
-      author='Department of Parks and Wildlife',
+      url='https://github.com/dbca-wa/dpaw-utils',
+      author='Department of Biodiversity, Conservation and Attractions',
       author_email='asi@dbca.wa.gov.au',
       license='BSD',
       packages=['dpaw_utils', 'dpaw_utils.requests'],
